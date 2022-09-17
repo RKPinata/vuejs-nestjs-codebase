ea2482d31e3013ac8fa158b6619ac7b67c9c37aa
Tan Vo <tanvo.bks2014@gmail.com>
2022-09-17 13:06:33 +0800

    Create authentication for app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as typeOrmConfig from './typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [AuthModule, UserModule, TodoModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    TodoModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from './service/user-info.service';
+import { UserController } from './user.controller';
+import { UserInfo } from './entity/user-info.entity';
 
-@Module({})
+@Module({
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+    TypeOrmModule.forFeature([UserInfo]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+})
 export class UserModule {}
