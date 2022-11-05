bdadc196b98147ae949cc244ff84584a6ade63af
Tan Vo <tanvo.bks2014@gmail.com>
2022-11-05 18:08:19 +0800

    Remove redundant code

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,20 +1,8 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Logger,
-  Patch,
-  Put,
-  UploadedFile,
-  UseGuards,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { GetUser } from '../auth/decorator/get-user.decorator';
 import { User } from '../auth/entity/user.entity';
-import { editFileName, imageFileFilter } from '../utils/file-upload.utils';
 import { userInfoData } from './interface/user-info.interface';
 import { UserService } from './service/user-info.service';
 import { UserInfoDto } from './dto/user-info.dto';
