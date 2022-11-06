75445348c40cb6d18b5499ecb2f90abd75b7e4df
Tan Vo <tanvo.bks2014@gmail.com>
2022-11-06 15:06:23 +0800

    Remove MulterModule

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './service/user-info.service';
 import { UserController } from './user.controller';
 import { UserInfoRepository } from './repository/user-info.repository';
 @Module({
-  imports: [
-    MulterModule.register({
-      dest: './uploads',
-    }),
-    TypeOrmModule.forFeature([UserInfoRepository]),
-  ],
+  imports: [TypeOrmModule.forFeature([UserInfoRepository])],
   controllers: [UserController],
   providers: [UserService],
 })
